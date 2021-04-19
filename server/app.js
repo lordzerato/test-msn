175ca56d5f54dfde2c80ebdf9ecdcfdb96901d5e
Fauzi Ramadan <fauzidanrama@gmail.com>
2021-04-19 23:58:20 +0700

    fixing error

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const router = require('./routes')
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(router)
+
+app.listen(PORT, () => {
+    console.log("App is running on port: " + PORT);
+})
